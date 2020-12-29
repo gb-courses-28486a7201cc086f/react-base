67026b770b9df2db73030ff2f641856344ac0ea3
Vitaly Shatskikh <vshatskikh@bellintegrator.ru>
2020-12-29 17:34:08 +0300

    Added MUI grid and app panel

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,18 +1,25 @@
 import React from "react";
+import Grid from '@material-ui/core/Grid';
 
 import { Header } from "./Header";
 import { ChatList } from "./ChatList";
 import { MessageContainer } from "./MessageContainer";
 import { MessageStore } from "../MessageStore";
 
+import "../styles/styles.css"
+
 export const Layout = (props) => {
     return (
-        <div className="layout">
-            <Header/>
-            <div className="content-container">
-                <ChatList/>
-                <MessageContainer store={new MessageStore()}/>
-            </div>
+        <div className="root">
+            <Header className="header"/>
+            <Grid container className="content-container">
+                <Grid item xs sm={4} lg={3} xl={2} className="chat-list">
+                    <ChatList/>
+                </Grid>
+                <Grid item xs={12} sm className="message-container">
+                    <MessageContainer store={new MessageStore()}/>
+                </Grid>
+            </Grid>
         </div>
     );
 }
