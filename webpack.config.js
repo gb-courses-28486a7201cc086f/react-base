c4b3a58dd2bd20e7b3471751f4396572a4aaa098
Vitaly Shatskikh <vshatskikh@bellintegrator.ru>
2020-12-30 09:44:22 +0300

    added devtool to webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,13 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
+    mode: "development",
     devServer: {
         host: "0.0.0.0",
         port: "8180",
         publicPath: "/static/build/"
     },
+    devtool: "inline-cheap-module-source-map",
     entry: {
         app: "./index.jsx"
     },
