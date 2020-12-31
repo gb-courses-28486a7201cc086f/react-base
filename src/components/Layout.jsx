741b8180de1793c804776443bde77bedb93d9356
Vitaly Shatskikh <vshatskikh@bellintegrator.ru>
2020-12-31 15:03:14 +0300

    added profile page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import Hidden from '@material-ui/core/Hidden';
 
 import { Header } from "./Header";
 import { ChatList } from "./ChatList";
@@ -11,6 +16,13 @@ import "../styles/styles.css"
 const messages = new MessageStore();
 // display first available chat by default
 const defaultChatId = Object.keys(messages.getChatTitles())[0];
+// get/add methods for message list 
+let getMessages = () => {
+    return messages.getMessages(defaultChatId);
+}
+let addMessage = (author, text) => {
+    return messages.addMessage(defaultChatId, author, text);
+} 
 
 /**
  * Top-level component, which contains header, chat list and message area
@@ -21,27 +33,47 @@ const defaultChatId = Object.keys(messages.getChatTitles())[0];
 export const Layout = (props) => {
     const currentChatId = props.chatId === undefined? defaultChatId : props.chatId;
     const titlesMap = messages.getChatTitles();
-    // chat title for header panel
-    const currentChatTitle = titlesMap[currentChatId];
-    // get/add methods for message list 
-    const getMessages = () => {
-        return messages.getMessages(currentChatId);
-    }
-    const addMessage = (author, text) => {
-        return messages.addMessage(currentChatId, author, text);
-    } 
     
-    // switch between chats list and messages on xs screens
-    //const
+    const [chatId, setChatId] = useState(currentChatId);
+    // update get/add methods when chat changed
+    useEffect(() => {
+        getMessages = () => {
+            return messages.getMessages(currentChatId);
+        }
+        addMessage = (author, text) => {
+            return messages.addMessage(currentChatId, author, text);
+        }
+        setChatId(currentChatId);
+    }, [currentChatId]);
+    
+    // TODO switch to chat list on xs screens
+    const backButton = (
+        <Hidden smUp>
+            <IconButton 
+                edge="start" 
+                color="inherit" 
+                aria-label="chats"
+                >
+                <ArrowBackIcon/>
+            </IconButton>
+        </Hidden>
+    );
+
+    const profileButton = (
+        <Link to={`${props.profileUri}`}>
+            <Button color="inherit">Profile</Button>
+        </Link>
+    );
 
     return (
         <div className="root">
             <Header 
-                chatTitle={currentChatTitle}
-                className="header"
+                title={`Chat: ${titlesMap[chatId]}`}
+                leftButton={backButton}
+                rightButton={profileButton}
                 />
             <Grid container className="content-container">
-                <Grid item xs={true} sm={4} lg={3} xl={2} className="chat-list">
+                <Grid item xs sm={4} xl={2} className="chat-list">
                     <ChatList 
                         chatTitles={titlesMap}
                         chatsBaseUri={props.chatsBaseUri}
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import { Layout } from './Layout';
+import { Profile } from './Profile';
 
 const chatsBase = "/chats";
+const profilePath = "/profile/";
 
 /**
  * Appication router
@@ -11,14 +13,19 @@ export const Router = (props) => {
     return (
         <Switch>
             <Route exact path="/" render={ 
-                () => <Layout chatsBaseUri={chatsBase}/>
+                () => <Layout 
+                        chatsBaseUri={chatsBase}
+                        profileUri={profilePath}
+                        />
             }/>
-            <Route path={`${chatsBase}/:chatId/`} render={ 
+            <Route exact path={`${chatsBase}/:chatId/`} render={ 
                 (props) => (<Layout 
                                 chatsBaseUri={chatsBase}
+                                profileUri={profilePath}
                                 chatId={props.match.params.chatId}
                                 />) 
             }/>
+            <Route exact path={profilePath} component={Profile} />
         </Switch>
     );
 }
