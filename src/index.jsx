f09c651ac11b6842d1553dd0337275c8e69c2b2c
Vitaly Shatskikh <vshatskikh@bellintegrator.ru>
2021-01-31 02:42:47 +0300

    persist gate connected

--- src/index.jsx
+++ src/index.jsx
@@ -3,21 +3,24 @@ import ReactDOM from "react-dom";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import { ConnectedRouter } from "connected-react-router";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import Router from "./components/Router";
 import initStore, { history } from "./store";
 
 import "./styles/styles.css"
 
-const store = initStore();
+const {store, persistor} = initStore();
 
 ReactDOM.render(
     <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <MuiThemeProvider>
-                <Router/>
-            </MuiThemeProvider>
-        </ConnectedRouter>
+        <PersistGate loading={null} persistor={persistor}>
+            <ConnectedRouter history={history}>
+                <MuiThemeProvider>
+                    <Router/>
+                </MuiThemeProvider>
+            </ConnectedRouter>
+        </PersistGate>
     </Provider>,
     document.getElementById("root")
 );
--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,22 @@ import { createBrowserHistory } from "history";
 import { routerMiddleware } from "connected-react-router";
 import reducers from "./reducers";
 import middlewares from "../middlewares";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+
+const persistConfig = {
+    key: 'MyMessanger',
+    storage,
+    stateReconciler: autoMergeLevel2,
+    whitelist: ['messageReducer', 'chatReducer'],
+ };
 
 export const history = createBrowserHistory();
 
 export default function initStore() {
-    return createStore(
-        reducers(history),
+    const store = createStore(
+        persistReducer(persistConfig, reducers(history)),
         compose(
             applyMiddleware(
                     routerMiddleware(history),
@@ -17,4 +27,8 @@ export default function initStore() {
             window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
         ),
     );
+
+    const persistor = persistStore(store);
+
+    return { store, persistor };
 }
